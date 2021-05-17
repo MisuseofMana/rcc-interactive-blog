6c960a8b4eec29d7359bb50c144098957fd71dca
MisuseofMana <contact@seanyager.com>
2021-05-17 10:49:45 -0500

    changed font sizes on page

--- pages/realms.vue
+++ pages/realms.vue
@@ -1,16 +1,18 @@
 <template>
     <v-container class="my-5">
         <v-row>
-            <v-col lg="3" md="4" sm="6" v-for="(posts, index) in pages" :key="index">
-                <div class="text-h4 primary--text">{{ posts.title }}</div>
-                <div class="text-h5 primary--text">{{ posts.subtitle }}</div>
-                <div class="border my-2">
-                    <NuxtLink :to="`insights/${posts.slug}`">
-                        <v-img :aspect-ratio="1/1" :src="require(`@/assets/images/${posts.imagesDirectory}/${posts.titlePhoto}.jpg`)" alt=""></v-img>
-                    </NuxtLink>
-                </div>
-                <div>
-                    <v-icon v-for="(items, index) in posts.iconNames" :key="items+index" color="primary">mdi-{{ items }}</v-icon>
+            <v-col lg="4" md="4" sm="6" v-for="(posts, index) in pages" :key="index">
+                <div class="border">
+                    <div class="text-h2">{{ posts.title }}</div>
+                    <div class="text-h3">{{ posts.subtitle }}</div>
+                    <div class="my-2 shadow">
+                        <NuxtLink :to="`insights/${posts.slug}`">
+                            <v-img class="realmImage" :aspect-ratio="1" :src="require(`@/assets/images/${posts.imagesDirectory}/${posts.titlePhoto}.jpg`)" alt=""></v-img>
+                        </NuxtLink>
+                    </div>
+                    <div>
+                        <v-icon v-for="(items, index) in posts.iconNames" :key="items+index">mdi-{{ items }}</v-icon>
+                    </div>
                 </div>
             </v-col>
         </v-row>
@@ -33,20 +35,29 @@
 
 <style scoped>
 .border {
-    border: solid 2px var(--v-primary-base);
-    width:100%;
-    overflow:hidden;
-    filter: hue-rotate(45deg);
+    /* background: var(--v-primary-darken3); */
+    transform:scale(1);
+    padding:30px;
+    z-index:-2;
     transition: all .25s;
+}
+
+.theme--light.v-icon, .border {
+     color: var(--v-primary-base);
 }
 
 .border:hover {
     cursor:pointer;
-    transform:scale(1.02);
+    padding:15px 16px;
     transition: all .25s;
 }
 
-img {
-    max-height:400px;
+.realmImage {
+    filter: contrast(125%) brightness(75%);
+    z-index:-1;
+}
+
+.shadow {
+    box-shadow: inset 20px 20px 25px var(--v-background-base), inset -20px -20px 25px var(--v-background-base);
 }
 </style>
