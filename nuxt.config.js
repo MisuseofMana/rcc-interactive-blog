da1efb3302b5129000422bd48386cafdfad261c0
MisuseofMana <contact@seanyager.com>
2021-05-17 10:43:58 -0500

    add new font

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,21 +1,31 @@
 import theme from './plugins/theme'
 
 export default {
+  head: {
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=VT323&display=swap'
+      }
+    ]
+  },
   server: {
     port: 8000 // default: 3000
   },
   css: [
     '~/assets/css/transitions.css',
-    '~/assets/css/globalReset.css',  
+    '~/assets/css/globalReset.css', 
+    '~/assets/css/scrollBar.css' 
   ],
   buildModules: [
     '@nuxtjs/vuetify'
   ],
   vuetify: {
+    customVariables: ['~/assets/variables.scss'],
     treeShake: true,
     defaultAssets: {
       font: {
-        family: 'Verdana',
+        family: 'VT323',
       },
       icons: 'mdi', 
     },
