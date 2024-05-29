493d5008b85769198b8366a312413ad7cf960db4
Sean Yager <contact@seanyager.com>
2024-05-29 00:40:03 -0400

    semiotic canvas to url implemented

--- components/display-components/SemioticImage.vue
+++ components/display-components/SemioticImage.vue
@@ -1,11 +1,18 @@
 <template>
-    <div>
+    <span>
         <canvas
+        class="d-none"
         ref="semioticCanvas"
         width="700"
         height="466"
         />
-    </div>
+        <v-img contain
+        :src="imageLink"
+        class="realmImage"
+        lazy-src="/images/mocks/placeholder-wide.jpg"
+        aspect-ratio="1.5"
+        />
+    </span>
 </template>
 
 <script setup>
@@ -14,21 +21,25 @@ const props = defineProps({
     photo: Object,
 })
 const semioticCanvas = ref()
+const imageLink = ref('')
 
 onMounted(() => {
     const ctx = semioticCanvas.value.getContext(`2d`)
 	const helperImg = new Image()
 	helperImg.src = props.photo.imageLink
 	ctx.globalCompositeOperation = 'normal'
+    helperImg.setAttribute('crossorigin', 'anonymous')
 	helperImg.onload = () => {
-		const overlay = new Image()
+        ctx.drawImage(helperImg, 0, 0, 700, 466)
+        const overlay = new Image()
 		overlay.src = `/images/semiotics/${props.photo.semioticValue}.png`
+        ctx.globalCompositeOperation = props.photo.renderMode
+        overlay.setAttribute('crossorigin', 'anonymous')
 		overlay.onload = () => {
-			ctx.globalCompositeOperation = props.photo.renderMode
 			ctx.drawImage(overlay, props.photo.overlayX, props.photo.overlayY, 100, 100);
+            const dataURL = semioticCanvas.value.toDataURL('image/webp')
+            imageLink.value = dataURL;
 		}
-		ctx.drawImage(helperImg, 0, 0, 700, 466)
-        const dataURL = ctx.toDataURL()
 	}
 })
 </script>
